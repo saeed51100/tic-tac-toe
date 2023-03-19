3a3f8631d5fbdbb22d8d83fa9272ab12f3c33e41
saeed javadzadeh <saeedblack77@gmail.com>
2023-03-19 18:15:27 +0330

    laptop 1401/12/28 12:33

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component }  from 'react';
+function Square({ value }) {
+    function handleClick() {
+        console.log('clicked!');
+    }
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}
 export default function Board() {
   return (
       <div>
-        <div className="board-row">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
-        </div>
-        <div className="board-row">
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
-        </div>
-        <div className="board-row">
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
-        </div>
+          <div className="board-row">
+              <Square value="1" />
+              <Square value="2" />
+              <Square value="3" />
+          </div>
+          <div className="board-row">
+              <Square value="4" />
+              <Square value="5" />
+              <Square value="6" />
+          </div>
+          <div className="board-row">
+              <Square value="7" />
+              <Square value="8" />
+              <Square value="9" />
+          </div>
       </div>
   );
 }
